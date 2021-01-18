e6f99416f1662f6594985e8792757d4166ca3ea3
Macaddict <dxbydt42@gmail.com>
2021-01-18 12:26:06 -0800

    Cleanup

--- FastModsModule.cs
+++ FastModsModule.cs
@@ -90,7 +90,7 @@ namespace AdvancedTooltip
                     i += modTierInfo.ModLines - 1;
                 }
             }
-            catch (Exception e)
+            catch (Exception)
             {
                 //ignored   
             }
